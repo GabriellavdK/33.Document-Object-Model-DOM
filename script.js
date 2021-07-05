a0a3838677d75a96647c02d43af6d38ddee0dc90
GabriellavdK <81296778+GabriellavdK@users.noreply.github.com>
2021-07-05 22:00:55 +0200

    Opdracht 33 geupload

---
+++ script.js
@@ -0,0 +1,11 @@
+// Deel 1
+// const paragraph = document.getElementsByTagName('p')
+// paragraph[0].innerHTML = "Ik ben een nieuwe paragraaf."
+
+// Deel 2
+// const firstSection = document.getElementById('first-section')
+// Vaak wordt er nog var gebruikt in Javascript, dit is ouderwets en kan beter vervangen worden door const of let. 
+// Const gebruik je wanneer je variabele niet meer wijzigt, en let wanneer dat wel het geval is. 
+
+// Deel 3
+// const paragrafen = document.getElementsByClassName('paragraph')
